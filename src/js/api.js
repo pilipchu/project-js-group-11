16b6214d3e87db2dbb2065fce562c436d593a81c
Al14lex <alenahomenko1421@gmail.com>
2024-04-11 23:50:24 +0200

    update
    Я винесла посилання в конст і прибрала трай-кетч. Якщо в понеділок це змінювати - то прийдеться переписувати всім хто  працює з запитами, щоб додати їм трай-кетч. Якщо щось не те, просто відхили зміни(

--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,12 @@
 // запит на сервер щоб отримати review
 
 import axios from "axios";
+import Swiper from "swiper";
 
- export async function getReviews() {
-  try {
-    const response = await axios.get("https://portfolio-js.b.goit.study/api/reviews");
+export async function getReviews() {
+  const url = "https://portfolio-js.b.goit.study/api/reviews";
+
+    const response = await axios.get (url);
 
     if (response.status !== 200) {
       throw new Error('Network response was not ok');
@@ -13,20 +15,17 @@ import axios from "axios";
     const data = response.data;
 
     return data;
-  } catch(error) {
-    console.error('Problem fetching the data:', error);
-    throw error;
-  }
 }
 
-getReviews();
+//getReviews()
 
 // =========================
 // тут метод POST
 
 export async function postReview(email, comment) {
-  try {
-    const response = await axios.post("https://portfolio-js.b.goit.study/api/requests", {
+  const postUrl = "https://portfolio-js.b.goit.study/api/requests";
+
+    const response = await axios.post(postUrl, {
       email: email,
       comment: comment
     });
@@ -34,26 +33,22 @@ export async function postReview(email, comment) {
     const responseData = response.data;
     // console.log(responseData); // теж розкоментуйте якщо треба глянути, що приходить
     return responseData;
-  } catch(error) {
-    console.error('Problem posting the review:', error);
-    throw error;
-  }
+
 }
 
 // накидаю приклад для перевірки роботи, бо в мене все норм. Якщо треба буде зробити перевірку - розкоментуйте два консти нижче)
-// const email = "its-work@gmail.com";
-// const comment = "Все працює. Всі молодці";
+//const email = "its-work@gmail.com";
+//const comment = "Все працює. Всі молодці";
 
-postReview(email, comment)
-  .catch(error => {
-    console.error(error);
-  });
+// postReview(email, comment)
+//   .catch(error => {
+//     console.error(error);
+//   });
 
 
 // ===================================================
 // SWIPER
 
-import Swiper from "swiper";
 
 export function initSwiper(containerId, direction, loop = false) {
   return new Swiper(containerId, {
