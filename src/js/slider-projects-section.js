b9fe62fc312d03b71ee1f122276d7969c7704711
angeange1 <149048056+angeange1@users.noreply.github.com>
2024-04-14 19:14:37 +0300

    pull-5-angela

--- src/js/slider-projects-section.js
+++ src/js/slider-projects-section.js
@@ -1,22 +1,37 @@
-const swiper = new Swiper('.swiper', {
+import Swiper from 'swiper'
+import 'swiper/css'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css/navigation'
+
+// import { initSwiper } from './api'
+
+// const projectsSwiper = initSwiper('#proj-swiper', 'horizontal', true);
+
+const swipeForwardBtn = document.querySelector(".swipe-forward-btn")
+const swipeBackBtn = document.querySelector(".swipe-back-btn")
+
+const swiper = new Swiper('#proj-swiper', {
     speed: 400,
-    spaceBetween: 100,
     direction: 'horizontal',
     loop: false,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-           return '<span class="' + className + '">' + (index + 1) + "</span>";
-         }
-  },
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: swipeForwardBtn,
+    prevEl: swipeBackBtn,
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+    enabled: true,
+    invert: true,
+  },
+  allowTouchMove: true,
+  followFinger: true,
+  simulateTouch: true,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  modules: [ Navigation],
+ 
 });
-
-const nextBtn = document.querySelector(".swiper-button-next")
-
-nextBtn.addEventListener("click", () => swiper.slideNext())
 
