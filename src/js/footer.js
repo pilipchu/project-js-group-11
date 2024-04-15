3606a4469ee12ce72b71de008da8db6a3f60205c
Podolianyk <podolianik1988@gmail.com>
2024-04-15 21:19:45 +0200

    update js

--- src/js/footer.js
+++ src/js/footer.js
@@ -3,48 +3,44 @@ const emailInput = document.getElementById('work-together-input-email');
 const commentInput = document.getElementById('work-together-input-text');
 const successMessage = document.getElementById('success-message');
 const invalidMessage = document.getElementById('invalid-message');
-const sendButton = document.querySelector('.work-together-btn');
 const popup = document.getElementById('work-together-popup');
 const closeModalButton = document.querySelector(
   '.work-together-modal-close-btn'
 );
 
+import axios from 'axios';
 import { postReview } from '../js/api.js';
 
-// !==================== ОБРІЗКА ТЕКСТУ (//обрізає на половині і не дає можливості видалити, тільки перезапуск сторінки)
-let maxLength =
-  Math.floor(
-    commentInput.clientWidth /
-      parseFloat(window.getComputedStyle(commentInput).fontSize)
-  ) - 1;
-commentInput.addEventListener('input', function () {
-  const currentLength = commentInput.value.length;
+//  ======================= Відправка форми
 
-  if (currentLength > maxLength) {
-    const truncatedText = commentInput.value.slice(0, maxLength);
-    const displayText = truncatedText + '...';
-    commentInput.value = displayText;
-  } else {
-    maxLength =
-      Math.floor(
-        commentInput.clientWidth /
-          parseFloat(window.getComputedStyle(commentInput).fontSize)
-      ) - 1;
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const email = emailInput.value;
+  const comment = commentInput.value;
+
+  if (!validateEmail(email)) {
+    showInvalidEmailMessage();
+    console.log(hi);
+    emailInput.focus();
   }
+  sendRequest(email, comment);
 });
 
-// !==================== Перевірка пошти, повертає true або false
+//  ==================== Перевірка пошти, повертає true або false
+
 function validateEmail(email) {
   const emailPattern = /^\w+(\.\w+)?@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
   return emailPattern.test(email);
 }
 
-// !==================== Post запит
+//  ==================== Post запит
+
 async function sendRequest(email, comment) {
   try {
     const response = await postReview(email, comment);
-    // console.log(response);
-    if (response.success) {
+    if (response) {
+      showSuccessMessage();
       popup.classList.add('open');
       emailInput.value = '';
       commentInput.value = '';
@@ -56,28 +52,8 @@ async function sendRequest(email, comment) {
   }
 }
 
-// !==================== Сплиття підсказки
-sendButton.addEventListener('click', function (event) {
-  event.preventDefault();
+// ==================== Закриття модального вікна
 
-  const email = emailInput.value;
-  const comment = commentInput.value;
-  if (!validateEmail(email)) {
-    invalidMessage.style.display = 'block';
-    successMessage.style.display = 'none';
-    emailInput.classList.remove('success');
-    emailInput.classList.add('remove');
-    emailInput.focus();
-    return;
-  }
-  emailInput.classList.remove('remove');
-  emailInput.classList.add('success');
-  successMessage.style.display = 'block';
-  invalidMessage.style.display = 'none';
-  sendRequest(email, comment);
-});
-
-// !==================== Закриття модального вікна
 closeModalButton.addEventListener('click', function () {
   closePopup();
 });
@@ -103,7 +79,41 @@ function closePopup() {
   commentInput.value = '';
 }
 
-//! ======================= Відкриття модального вікна за кліком, в коді не потрібен, для перевірки
-// sendButton.addEventListener('click', function (e) {
-//   popup.classList.add('open');
-// });
+// =======================  Невалідна електрона пошта
+function showInvalidEmailMessage() {
+  invalidMessage.style.display = 'block';
+  successMessage.style.display = 'none';
+  emailInput.classList.remove('success');
+  emailInput.classList.add('remove');
+}
+
+// ======================== Валідна електрона пошта
+function showSuccessMessage() {
+  successMessage.style.display = 'block';
+  invalidMessage.style.display = 'none';
+  emailInput.classList.remove('remove');
+  emailInput.classList.add('success');
+}
+
+// ==================== Обрізка тексту
+//! обрізає на половині і не дає можливості видалити, тільки перезапуск сторінки
+let maxLength =
+  Math.floor(
+    commentInput.clientWidth /
+      parseFloat(window.getComputedStyle(commentInput).fontSize)
+  ) - 1;
+commentInput.addEventListener('input', function () {
+  const currentLength = commentInput.value.length;
+
+  if (currentLength > maxLength) {
+    const truncatedText = commentInput.value.slice(0, maxLength);
+    const displayText = truncatedText + '...';
+    commentInput.value = displayText;
+  } else {
+    maxLength =
+      Math.floor(
+        commentInput.clientWidth /
+          parseFloat(window.getComputedStyle(commentInput).fontSize)
+      ) - 1;
+  }
+});
