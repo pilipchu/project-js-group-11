e71d89199137076b01ddc5e506398411585e2e1f
Al14lex <alenahomenko1421@gmail.com>
2024-04-17 01:31:41 +0200

    open/close mobile-menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -2,7 +2,8 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-    const closeMenuLink = document.querySelectorAll('.header-link');
+  const closeMenuLink = document.querySelectorAll('.header-link');
+  const orderBtn = document.querySelector('.js-order-btn');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,8 +18,9 @@
   closeMenuLink.forEach(item => item.addEventListener('click', toggleMenu));
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  orderBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
+ 
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
